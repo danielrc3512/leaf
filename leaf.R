0b197fe2df60579fe7d91312c455890314d04c3e
Daniel Ruiz Casanova <danielrc3512@msn.com>
2014-05-24 12:52:23 +0200

    Versión inicial

--- leaf.R
+++ leaf.R
@@ -1,9 +1,46 @@
 
+#datos <- "C:/Users/Danielin/Documents/GitHub/leaf/leaf.csv"
+datos <- "https://github.com/danielrc3512/leaf/blob/master/leaf.csv"
+misdatos <- read.csv(datos)
 mediaMaximalIndentationDepth <- function ()
 {
 
-	misdatos <- read.csv("C:/Users/Danielin/Desktop/R/leaf/leaf.csv")
 	maximalIndentationDeph <- misdatos[,9]
 	median(maximalIndentationDeph)
+
+}
+mediaXClase <- function ()
+{
+
+ largo <- dim(misdatos)[1]
+ tClase <- 0
+ sClase <- 0
+ for (i in 1:largo )
+ {
+	clase <- misdatos[i,1]
+	sClase <- sClase + misdatos[i,9]
+	tClase <- tClase + 1
+	
+	if ( i == largo) 
+	{
+		texto <- paste("La media Maximal Indentation Depth de la clase", clase,"es",(sClase / tClase), sep = " ", collapse = NULL) 
+		print(texto)
+			
+	} else {
+	
+		if (clase != misdatos[i+1,1] )
+		{
+		
+		
+			texto <- paste("La media Maximal Indentation Depth de la clase", clase,"es",(sClase / tClase), sep = " ", collapse = NULL) 
+			print(texto)
+			tClase <- 0
+			sClase <- 0
+		
+		} 
+	}
+ }
+
+
 
 }
